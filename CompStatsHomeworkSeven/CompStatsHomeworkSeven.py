e68c121f3fe4ea4283ff93adb6bad11785218efb
root <rmhutchins@gmail.com>
2020-06-16 15:29:26 +0200

    parts a, b, and c

--- CompStatsHomeworkSeven/CompStatsHomeworkSeven.py
+++ CompStatsHomeworkSeven/CompStatsHomeworkSeven.py
@@ -1,10 +1,23 @@
+from __future__ import print_function
+
 import numpy as np
-import pandas
 import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 from numpy.linalg import inv
+from torch import Tensor
+from torch.nn import Sequential
 from typing import List
 from typing import Tuple
+
+
+activation_function_dictionary = {
+    "linear": torch.nn.Linear,
+    "relu": torch.nn.ReLU,
+    "sigmoid": torch.nn.Sigmoid
+}
 
 
 def get_data(filename) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -58,8 +71,153 @@ def do_part_a(filename: str, degree: int):
     plt.show()
 
 
+def capture_data_as_torch_tensors(filename) -> Tuple[Tensor, Tensor]:
+    # Create random Tensors to hold inputs and outputs
+    with open(filename, "r") as f:
+        lines: List[str] = f.readlines()[1:]
+        xs: np.ndarray = np.array([float(line.split(",")[0]) for line in lines])
+        ys: np.ndarray = np.array([float(line.split(",")[1].strip()) for line in lines])
+        x = torch.from_numpy(xs).reshape((200, 1))
+        y = torch.from_numpy(ys).reshape((200, 1))
+    return x, y
 
 
+def train_model(x, y, model: Sequential, loss_fn, learning_rate) -> Tuple[Sequential, float]:
+    loss = None
+    for t in range(20000):
+        # Forward pass: compute predicted y by passing x to the model. Module objects
+        # override the __call__ operator so you can call them like functions. When
+        # doing so you pass a Tensor of input data to the Module and it produces
+        # a Tensor of output data.
+        y_pred = model(x.float()).double()
 
-file = "/Users/administrator/PycharmProjects/ComputationalStatistics/CompStatsHomeworkSeven/data/sheet7.csv"
+        # Compute and print loss. We pass Tensors containing the predicted and true
+        # values of y, and the loss function returns a Tensor containing the
+        # loss.
+        loss = loss_fn(y_pred, y).double()
+        if t % 10000 == 9999:
+            print(t, loss.item())
+
+        # Zero the gradients before running the backward pass.
+        model.zero_grad()
+
+        # Backward pass: compute gradient of the loss with respect to all the learnable
+        # parameters of the model. Internally, the parameters of each Module are stored
+        # in Tensors which requires_grad=True, so this call will compute gradients for
+        # all learnable parameters in the model.
+        loss.backward()
+
+        # Update the weights using gradient descent. Each parameter is a Tensor, so
+        # we can access its gradients like we did before.
+        with torch.no_grad():
+            for param in model.parameters():
+                param -= learning_rate * param.grad
+
+    return model, loss.item()
+
+
+def plot_results(x, y, y_predicted):
+    plt.scatter(x.detach().numpy(), y.detach().numpy(), color="b", label="raw data")
+    plt.plot(x.detach().numpy(), y_predicted.detach().numpy(), color="r", label="predicted")
+    plt.legend()
+    plt.show()
+
+
+def construct_neural_network_model(
+        input_dimension: int,
+        output_dimension: int,
+        hidden_layers: List[Tuple[int, str]],
+) -> Sequential:
+    model: Sequential = Sequential()
+    print(f"input dimension = {input_dimension}")
+    for i, hidden_layer in enumerate(hidden_layers):
+        print(f"i = {i}, hidden layer = {hidden_layer}")
+        print(f"current_layer_dimension = {hidden_layers[i][0]}")
+        if i == 0:
+            input_layer_module = Sequential(
+                torch.nn.Linear(input_dimension, hidden_layers[i][0]),
+                activation_function_dictionary[hidden_layers[i][1]]()
+            )
+            model.add_module("input_layer", input_layer_module)
+        if i < len(hidden_layers) - 1:
+            out_dim = hidden_layers[i+1][0]
+            hidden_layer_module = Sequential(
+                torch.nn.Linear(hidden_layers[i][0], out_dim, bias=True),
+                activation_function_dictionary[hidden_layers[i][1]]()
+            )
+            print(f"next hidden layer dimension = {out_dim}")
+        else:
+            out_dim = output_dimension
+            hidden_layer_module = Sequential(torch.nn.Linear(hidden_layers[i][0], out_dim, bias=True))
+
+        model.add_module(f"hidden_layer_{i}", hidden_layer_module)
+
+    print(model)
+    return model
+
+
+def execute(
+        filename: str,
+        input_dimension: int,
+        output_dimension: int,
+        hidden_layers: List[Tuple[int, str]],
+        loss_fn,
+        learning_rate
+) -> Tuple[Sequential, float]:
+    model: Sequential = construct_neural_network_model(
+        input_dimension=input_dimension,
+        output_dimension=output_dimension,
+        hidden_layers=hidden_layers
+    )
+    x, y = capture_data_as_torch_tensors(filename=filename)
+    model, loss = execute_neural_network_model(x=x, y=y, model=model, loss_fn=loss_fn, learning_rate=learning_rate)
+    return model, loss
+
+
+def execute_neural_network_model(
+        x: torch.Tensor,
+        y: torch.Tensor,
+        model: Sequential,
+        loss_fn,
+        learning_rate
+) -> Tuple[Sequential, float]:
+    model, loss = train_model(x=x, y=y, model=model, loss_fn=loss_fn, learning_rate=learning_rate)
+    return model, loss
+
+
+def do_part_b(filename: str):
+    model, loss = execute(
+        filename=filename,
+        input_dimension=1,
+        output_dimension=1,
+        hidden_layers=[(7, "relu")],
+        loss_fn=torch.nn.MSELoss(reduction='sum'),
+        learning_rate=1e-5
+    )
+
+    x, y = capture_data_as_torch_tensors(filename=filename)
+    y_predicted_final = model(x.float())
+
+    plot_results(x=x, y=y, y_predicted=y_predicted_final)
+
+
+def do_part_c(filename: str):
+    model, loss = execute(
+        filename=filename,
+        input_dimension=1,
+        output_dimension=1,
+        hidden_layers=[(3, "relu"), (3, "relu")],
+        loss_fn=torch.nn.MSELoss(reduction='sum'),
+        learning_rate=1e-5
+    )
+
+    x, y = capture_data_as_torch_tensors(filename=filename)
+    y_predicted_final = model(x.float())
+
+    plot_results(x=x, y=y, y_predicted=y_predicted_final)
+
+
+file = "/home/ryan/PycharmProjects/ComputationalStatistics/CompStatsHomeworkSeven/data/sheet7.csv"
 do_part_a(filename=file, degree=21)
+do_part_b(filename=file)
+do_part_c(filename=file)
